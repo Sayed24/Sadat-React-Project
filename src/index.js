818f017e5c1623a884620c22b4539f9d0e62ab23
Sayed Sadat <SadatSayed92@gmail.com>
2023-04-11 17:59:48 -0700

    Setting up CSS and Responsiveness for different devicess

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import { clear } from '@testing-library/user-event/dist/clear';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
